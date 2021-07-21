b357b7b888eb6a059fbf459ba3b670164ca8a949
Evans Osinaike <the.evans.osinaike@gmail.com>
2021-07-21 21:51:38 +0100

    spelling correction

--- Create an HTML List.py
+++ Create an HTML List.py
@@ -5,6 +5,6 @@ html_str = "<ul>\n"  # "\ n" is the character that marks the end of the line, it
 # write your code here
 for item in items:
     html_str += f'<li>{item}</li>\n'
-    #html_str += '<li>{}</li>\n'.format(item) #Alternat solution
+    #html_str += '<li>{}</li>\n'.format(item) #Alternate solution
 html_str += "</ul>"
 print(html_str)
