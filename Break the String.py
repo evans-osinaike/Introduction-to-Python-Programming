9b799806156157a1c1db3d11512b8d204dd57395
Osinaike Evans <the.evans.osinaike@gmail.com>
2021-07-22 23:32:08 +0100

    solved the break-continue lesson quiz

---
+++ Break the String.py
@@ -0,0 +1,24 @@
+# HINT: modify the headlines list to verify your loop works with different inputs
+headlines = ["Local Bear Eaten by Man",
+             "Legislature Announces New Laws",
+             "Peasant Discovers Violence Inherent in System",
+             "Cat Rescues Fireman Stuck in Tree",
+             "Brave Knight Runs Away",
+             "Papperbok Review: Totally Triffic"]
+
+news_ticker = ""
+
+# write your loop here
+for headline in headlines:
+    #print(f'Current length of ticker is: {len(news_ticker)}')
+    if len(news_ticker) + len(headline) < (140-1):
+        news_ticker += f'{headline} '
+        #print(f'Current length of ticker is: {len(news_ticker)}')
+    else:
+        for char in headline:
+            if len(news_ticker) < (140):
+                news_ticker += char
+            else: 
+                break
+
+print(news_ticker)
