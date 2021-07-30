60ba3c65cc28476bda1bdb5a62ce2551625ff9a2
Evans Osinaike <the.evans.osinaike@gmail.com>
2021-07-30 14:54:48 +0100

    solved Q1b

--- PQ 1.py
+++ PQ 1.py
@@ -2,18 +2,15 @@ nominated = {1931: ['Norman Taurog', 'Wesley Ruggles', 'Clarence Brown', 'Lewis 
 winners = {1931: ['Norman Taurog'], 1932: ['Frank Borzage'], 1933: ['Frank Lloyd'], 1934: ['Frank Capra'], 1935: ['John Ford'], 1936: ['Frank Capra'], 1937: ['Leo McCarey'], 1938: ['Frank Capra'], 1939: ['Victor Fleming'], 1940: ['John Ford'], 1941: ['John Ford'], 1942: ['William Wyler'], 1943: ['Michael Curtiz'], 1944: ['Leo McCarey'], 1945: ['Billy Wilder'], 1946: ['William Wyler'], 1947: ['Elia Kazan'], 1948: ['John Huston'], 1949: ['Joseph L. Mankiewicz'], 1950: ['Joseph L. Mankiewicz'], 1951: ['George Stevens'], 1952: ['John Ford'], 1953: ['Fred Zinnemann'], 1954: ['Elia Kazan'], 1955: ['Delbert Mann'], 1956: ['George Stevens'], 1957: ['David Lean'], 1958: ['Vincente Minnelli'], 1959: ['William Wyler'], 1960: ['Billy Wilder'], 1961: ['Jerome Robbins', 'Robert Wise'], 1962: ['David Lean'], 1963: ['Tony Richardson'], 1964: ['George Cukor'], 1965: ['Robert Wise'], 1966: ['Fred Zinnemann'], 1967: ['Mike Nichols'], 1968: ['Carol Reed'], 1969: ['John Schlesinger'], 1970: ['Franklin J. Schaffner'], 1971: ['William Friedkin'], 1972: ['Bob Fosse'], 1973: ['George Roy Hill'], 1974: ['Francis Ford Coppola'], 1975: ['Milos Forman'], 1976: ['John G. Avildsen'], 1977: ['Woody Allen'], 1978: ['Michael Cimino'], 1979: ['Robert Benton'], 1980: ['Robert Redford'], 1981: ['Warren Beatty'], 1982: ['Richard Attenborough'], 1983: ['James L. Brooks'], 1984: ['Milos Forman'], 1985: ['Sydney Pollack'], 1986: ['Oliver Stone'], 1987: ['Bernardo Bertolucci'], 1988: ['Barry Levinson'], 1989: ['Oliver Stone'], 1990: ['Kevin Costner'], 1991: ['Jonathan Demme'], 1992: ['Clint Eastwood'], 1993: ['Steven Spielberg'], 1994: ['Robert Zemeckis'], 1995: ['Mel Gibson'], 1996: ['Anthony Minghella'], 1997: ['James Cameron'], 1998: ['Steven Spielberg'], 1999: ['Sam Mendes'], 2000: ['Steven Soderbergh'], 2001: ['Ron Howard'], 2002: ['Roman Polanski'], 2003: ['Peter Jackson'], 2004: ['Clint Eastwood'], 2005: ['Ang Lee'], 2006: ['Martin Scorsese'], 2007: ['Ethan Coen', 'Joel Coen'], 2008: ['Danny Boyle'], 2009: ['Kathryn Bigelow'], 2010: ['Tom Hooper']}
 
 ### Question 1A: Create dictionary with the count of Oscar nominations for each director 
-#all_names1 = sum([nom_name for year, nom_name in nominated.items()], [])
 
-all_names = [nom_name for year, nom_names in nominated.items() for nom_name in nom_names]
+#Create a list of all the nominations regardless of repetition)
+#all_names1 = sum([nom_name for year, nom_name in nominated.items()], [])
+all_noms = [nom_name for year, nom_names in nominated.items() for nom_name in nom_names]
 
 #print(all_names1 == all_names)
 nom_count_dict = {}
-for name in set(all_names):
-    nom_count_dict[name] = all_names.count(name)
-
-# Add your solution code below before line 10. Add more lines for your code as needed.
-
-
+for name in set(all_noms):
+    nom_count_dict[name] = all_noms.count(name)
 
 print("nom_count_dict = {}\n".format(nom_count_dict))
 ###################################################################################################################
@@ -21,8 +18,8 @@ print("nom_count_dict = {}\n".format(nom_count_dict))
 
 ### Question 1B: Create dictionary with the count of Oscar wins for each director
 win_count_dict = {}
-# Add your solution code below before line 20. Add more lines for your code as needed.
-
-
+all_winners = [str(name) for year, name in winners.items()]
+for name in set(all_winners):
+    win_count_dict[name] = all_winners.count(name)
 
 print("win_count_dict = {}".format(win_count_dict))
