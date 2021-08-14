93fff61ff301221c27ea8eb1314a6ce7691e64ab
Evans Osinaike <the.evans.osinaike@gmail.com>
2021-08-14 11:29:53 +0100

    reading and writing files update

--- Reading and writing files.py
+++ Reading and writing files.py
@@ -11,3 +11,7 @@ f = open('some_file2.txt', 'w')
 f.write("Hello There! \n\nLet\'s keep creating magic together. \n\n Cheers")
 f.close()
 
+with open('some_file2.txt', 'r') as f:
+    file_data = f.read()
+
+print(file_data)
