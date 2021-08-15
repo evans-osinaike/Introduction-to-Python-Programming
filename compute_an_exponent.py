89c944db3cb15f3576cbd7d144ccb42126fe3eea
Evans Osinaike <the.evans.osinaike@gmail.com>
2021-08-15 01:17:49 +0100

    imported the randrange module and created a password generator

---
+++ compute_an_exponent.py
@@ -0,0 +1,6 @@
+import math as m
+def main():
+    print(m.exp(3))
+
+if __name__ == '__main__':
+    main()
---
+++ password_generator.py
@@ -0,0 +1,33 @@
+# TODO: First import the `random` module
+import random as r
+
+# We begin with an empty `word_list`
+word_file = "word_list.txt"
+word_list = []
+
+# We fill up the word_list from the `word_list.txt` file
+with open(word_file,'r') as words:
+	for line in words:
+		# remove white space and make everything lowercase
+		word = line.strip().lower()
+		# don't include words that are too long or too short
+		if 3 < len(word) < 8:
+			word_list.append(word)
+
+# TODO: Add your function generate_password below
+# It should return a string consisting of three random words 
+# concatenated together without spaces
+
+def generate_password(word_list):
+    count = 0 #Iteration counter
+    password = ''
+    while count < 3:
+        #pick random index on the list
+        i = r.randrange(0,len(word_list))
+        # update the current password phrase
+        password += word_list[i]
+        count += 1
+    return password
+
+# Now we test the function
+print(generate_password(word_list))
